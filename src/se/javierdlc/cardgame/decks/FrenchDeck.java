6a050bdc05a794a7d2507b01af7c54e548ddeee4
Javier De Las Casas <javier.delascasaschung@gmail.com>
2018-07-23 23:06:58 +0200

    Add Class for the French style deck

--- src/se/javierdlc/cardgame/decks/FrenchDeck.java
+++ src/se/javierdlc/cardgame/decks/FrenchDeck.java
@@ -1,4 +1,31 @@
 package se.javierdlc.cardgame.decks;
 
-public class FrenchDeck {
+import se.javierdlc.cardobjects.Card;
+import se.javierdlc.cardobjects.Deck;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FrenchDeck extends Deck {
+    final private List<Card> STANDARD_FRENCHDECK = getStandard();
+    private List<FrenchCard> cards;
+
+    public FrenchDeck(){
+        this.setCardsInPlay(STANDARD_FRENCHDECK);
+    }
+
+    private List<Card> getStandard(){
+        List<Card> cards = new ArrayList<>();
+        for (int i = 1; i < 14; i++) {
+            Card newCard = new FrenchCard(FrenchCategories.HEARTS, i);
+            cards.add(newCard);
+            newCard = new FrenchCard(FrenchCategories.SPADES,i);
+            cards.add(newCard);
+            newCard = new FrenchCard(FrenchCategories.DIAMONDS, i);
+            cards.add(newCard);
+            newCard = new FrenchCard(FrenchCategories.CLOVES, i);
+            cards.add(newCard);
+        }
+        return cards;
+    }
 }
