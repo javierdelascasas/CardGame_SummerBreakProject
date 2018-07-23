dd69103970deef253c4a603ee558e5058c5efb3e
Javier De Las Casas <javier.delascasaschung@gmail.com>
2018-07-23 23:08:22 +0200

    Add enum for French style Card categories

---
+++ src/se/javierdlc/cardgame/decks/FrenchCategories.java
@@ -0,0 +1,10 @@
+package se.javierdlc.cardgame.decks;
+
+public enum FrenchCategories {
+    HEARTS(1), DIAMONDS(2), SPADES(3), CLOVES(4);
+    private int value;
+
+    FrenchCategories(int value){
+        this.value = value;
+    }
+}
